0c10a41132228604c4f3ace1499b866ef7e08eda
yoshifumimiya <49806292+YoshifumiMiyagi@users.noreply.github.com>
2026-09-23 21:52:19 +0900

    Add fontan_manifold package exports

---
+++ src/fontan_manifold/__init__.py
@@ -0,0 +1,19 @@
+from .core import (
+    FontanManifold,
+    FontanOutcomeAnalyzer,
+    ProjectionResult,
+    default_sex_binary,
+    DEFAULT_DOMAIN_DEFINITION,
+    DEFAULT_CONTINUOUS_MI_VARS,
+    DEFAULT_OUTCOMES,
+)
+
+__all__ = [
+    "FontanManifold",
+    "FontanOutcomeAnalyzer",
+    "ProjectionResult",
+    "default_sex_binary",
+    "DEFAULT_DOMAIN_DEFINITION",
+    "DEFAULT_CONTINUOUS_MI_VARS",
+    "DEFAULT_OUTCOMES",
+]
